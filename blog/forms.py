f8707360642c8ebee688e79f69a20387faafad27
LTC <yokolautszching@Gmail.com>
2020-10-21 18:18:49 +0800

    Update forms.py

---
+++ blog/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Article, Tag, Comment, Blogger
+
+TYPE_CHOICES = ('Philosophy', 'Science', 'Art', 'Technology', 'Relationships')
+
+
+class ArticleForm(forms.Form):
+    title = forms.CharField(help_text="Enter the title.", max_length=20)
+    content = forms.CharField(widget=forms.Textarea)
+    date_time = forms.DateField
+    about = forms.MultipleChoiceField(
+            required=False,
+            widget=forms.CheckboxSelectMultiple,
+            choices=TYPE_CHOICES,)
+
+    title.widget.attrs.update({'class': 'special'}, size='40')
+
+
